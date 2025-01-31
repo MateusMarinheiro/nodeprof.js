99b16a41961452e6f737ad1521254ba9c3f58a27
Mateus Marinheiro <72341095+MateusMarinheiro@users.noreply.github.com>
2025-01-31 13:59:55 +0100

    fix: updated settings to be compatible with new dependencies versions

--- src/ch.usi.inf.nodeprof/src/ch/usi/inf/nodeprof/jalangi/factory/InitialRootFactory.java
+++ src/ch.usi.inf.nodeprof/src/ch/usi/inf/nodeprof/jalangi/factory/InitialRootFactory.java
@@ -54,7 +54,7 @@ public class InitialRootFactory extends AbstractFactory {
         }
         if (seenSources.add(source)) {
             // getCharacters() needs to be behind boundary
-            return Strings.fromCharSequence(source.getCharacters());
+            return Strings.fromJavaString(source.getCharacters().toString());
         }
         return null;
     }
